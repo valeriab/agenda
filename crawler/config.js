c42aa39734561bc76f73b4e7d5fcbb7c06f84f98
Valeria Boshnakova <valeria.boshnakova@cleverbase.com>
2018-02-10 20:25:11 +0100

    Change gist URL

--- crawler/config.js
+++ crawler/config.js
@@ -7,15 +7,26 @@ module.exports.sources = ["com.meetup", "com.eventbrite"];
  */
 module.exports.meetupGroups = {
   [18667416]: "Coding Value Zoetermeer",
-  [14194182]: "Dev 070",
-  [11093492]: "Docker Randstad",
-  [18611398]: "Functional Rotterdam",
-  [18822858]: "Developers NL Meetup",
-  [1759253]: "/dev/Haag",
-  [24197985]: "KPN CloudGURUS",
-  [4878142]: "AmsterdamJS",
   [21517897]: "Zoetermeer technight",
-  [20144457]: "Scrum Round Table"
+  [1759253]: "/dev/Haag",
+  [14194182]: "Dev 070",
+  [18611398]: "Functional Rotterdam",
+  [19002526]: "Reactive Amsterdam",
+  [6669802]: "Amsterdam Scala",
+  [8117202]: "Dutch scala enthusiasts",
+  [18822858]: "Developers NL Meetup",
+  [24197985]: "KPN CloudGURUS",
+  [19107935]: "Hack de overheid",
+  [11093492]: "Docker Randstad",
+  [4878142]: "AmsterdamJS",
+  [17413992]: "Frontend Developer Meetup Amsterdam",
+  [17202092]: "React Amsterdam",
+  [20128179]: "Front-end Forward",
+  [19446994]: "Amsterdam Webdevelopment Masterclasses",
+  [20144457]: "Scrum Round Table",
+  [18407783]: "Amsterdam Legal Hackers",
+  [8865472]: "Amsterdam UX",
+  [18994920]: "Ladies that UX"
 };
 
-module.exports.eventbriteOrganisers = ["hackages-13629245528"];
+module.exports.eventbriteOrganisers = ["react-amsterdam-8745897714"];
